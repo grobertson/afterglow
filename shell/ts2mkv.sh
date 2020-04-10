7cca13859321a874515bacfe70977240463841ca
Forrest Robertson <forrest.robertson5@t-mobile.com>
2020-04-10 08:16:27 -0700

    bulk scan and transcode script

--- shell/ts2mkv.sh
+++ shell/ts2mkv.sh
@@ -2,7 +2,7 @@
 
 # Run post processing script on each .ts file
 
-find /Recording /media /media2 -iname '*.ts' -exec /opt/bin/afterglow-post-processing "{}" \;
+find /Recording /media /media2 -iname '*.ts' -exec /opt/bin/afterglow-post-processing '{}' \;
 
 
 find . -iname '*.ts' -exec echo "{}" >> /tmp/ts_files.txt \;
