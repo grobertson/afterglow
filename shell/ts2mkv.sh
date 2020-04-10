82bd55e2deb0cbab4dd65624a9668715cf594080
Forrest Robertson <forrest.robertson5@t-mobile.com>
2020-04-10 08:03:50 -0700

    utils

--- shell/ts2mkv.sh
+++ shell/ts2mkv.sh
@@ -2,7 +2,9 @@
 
 # Run post processing script on each .ts file
 
-find . -iname '*.ts' -exec /opt/bin/post-processing.sh "{}" \;
+find /Recording /media /media2 -iname '*.ts' -exec /opt/bin/afterglow-post-processing "{}" \;
 
 
 find . -iname '*.ts' -exec echo "{}" >> /tmp/ts_files.txt \;
+
+find /Recording /media /media2 -iname '*.ts' -printf "%s\n" >> /tmp/ts_sizes.txt;
